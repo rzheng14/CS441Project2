d6c7f31c0c40b487b401f4204d5a398d0af66314
Richard Zheng <Richard@Richards-MacBook-Pro-5.local>
2019-02-13 17:31:05 -0500

    no animations yet

--- 2048/2048/ViewController.m
+++ 2048/2048/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <time.h>
+#import <stdlib.h>
 
 @interface ViewController ()
 
@@ -27,6 +29,9 @@ int counter;
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+ 
+  [self initSpawn];
+  [self spawn];
   
   UISwipeGestureRecognizer *down = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipe:)];
   down.direction = UISwipeGestureRecognizerDirectionDown;
@@ -48,14 +53,17 @@ int counter;
 -(void)keepScore {
   counter = t1.text.intValue + t2.text.intValue;
   [score setText:[NSString stringWithFormat: @"%d", counter]];
+  if(counter >= 1048576) {
+     [score setText:[NSString stringWithFormat: @"Pls Stop"]];
+  }
 }
 
 - (IBAction)changeVal:(id)sender {
   //new Game button. I'm not sure why it wont let me change the name of this function.
   [t1 setText:[NSString stringWithFormat: @"%d", 2]];
   [t2 setText:[NSString stringWithFormat: @"%d", 2]];
-  t1.center = CGPointMake(255.5, 438.5); //moves to initial starting spot
-  t2.center = CGPointMake(158.5, 438.5); //moves to initial starting spot
+  [self initSpawn];
+  [self spawn];
   [self keepScore];
 }
 
@@ -63,7 +71,7 @@ int counter;
 //gesture recognizer to read the swipes
 -(void)swipe:(UISwipeGestureRecognizer *) sender {
   
-  [self combine];  //works but need to keep swiping for it to update
+  //[self combine];  //works but need to keep swiping for it to update
   if(sender.direction == UISwipeGestureRecognizerDirectionDown) {
     [self moveTiles:@"DOWN"];
   }
@@ -76,6 +84,7 @@ int counter;
   if(sender.direction == UISwipeGestureRecognizerDirectionLeft) {
     [self moveTiles:@"LEFT"];
   }
+  [self combine]; //much better :)
 }
 
 //moves tiles
@@ -83,23 +92,23 @@ int counter;
   //moves the tiles and also keeps them on the screen
   if([direction isEqualToString:@"DOWN"]) {
     //move down
-    if(t1.center.y <= 677) t1.center = CGPointMake(t1.center.x, t1.center.y + 91);
-    if(t2.center.y <= 677) t2.center = CGPointMake(t2.center.x, t2.center.y + 91);
+    while(t1.center.y <= 677) t1.center = CGPointMake(t1.center.x, t1.center.y + 91);
+    while(t2.center.y <= 677) t2.center = CGPointMake(t2.center.x, t2.center.y + 91);
   }
   if([direction isEqualToString:@"RIGHT"]) {
     //move right
-    if(t1.center.x <= 318) t1.center = CGPointMake(t1.center.x+97, t1.center.y);
-    if(t2.center.x <= 318) t2.center = CGPointMake(t2.center.x+97, t2.center.y);
+    while(t1.center.x <= 318) t1.center = CGPointMake(t1.center.x+97, t1.center.y);
+    while(t2.center.x <= 318) t2.center = CGPointMake(t2.center.x+97, t2.center.y);
   }
   if([direction isEqualToString:@"UP"]) {
     //move up
-    if(t1.center.y >= 495) t1.center = CGPointMake(t1.center.x, t1.center.y - 91);
-    if(t2.center.y >= 495) t2.center = CGPointMake(t2.center.x, t2.center.y - 91);
+    while(t1.center.y >= 495) t1.center = CGPointMake(t1.center.x, t1.center.y - 91);
+    while(t2.center.y >= 495) t2.center = CGPointMake(t2.center.x, t2.center.y - 91);
   }
   if([direction isEqualToString:@"LEFT"]) {
     //move left
-    if(t1.center.x >= 124) t1.center = CGPointMake(t1.center.x-97, t1.center.y);
-    if(t2.center.x >= 124) t2.center = CGPointMake(t2.center.x-97, t2.center.y);
+    while(t1.center.x >= 124) t1.center = CGPointMake(t1.center.x-97, t1.center.y);
+    while(t2.center.x >= 124) t2.center = CGPointMake(t2.center.x-97, t2.center.y);
   }
 }
 
@@ -107,10 +116,87 @@ int counter;
   int value = t1.text.intValue * 2;
   if((t1.center.x == t2.center.x) && (t1.center.y == t2.center.y)) {
     [t1 setText:[NSString stringWithFormat:@"%d", value]];
-    t2.center = CGPointMake(158.5, 438.5);
+    //t2.center = CGPointMake(158.5, 438.5); //spawns in this spot. change
+    [self spawn];
     [t2 setText:[NSString stringWithFormat:@"%d", value]];
   }
   [self keepScore];
+}
+
+-(void)spawn {
+  int squareNum = arc4random_uniform(16);
+  
+  if(squareNum == 0) {
+    t2.center = CGPointMake(61.5, 438.5);
+  } else if (squareNum == 1) {
+    t2.center = CGPointMake(158.5, 438.5);
+  } else if (squareNum == 2) {
+    t2.center = CGPointMake(255.5, 438.5);
+  } else if (squareNum == 3) {
+    t2.center = CGPointMake(352.5, 438.5);
+  } else if (squareNum == 4) {
+    t2.center = CGPointMake(61.5, 529.5);
+  } else if (squareNum == 5) {
+    t2.center = CGPointMake(158.5, 529.5);
+  } else if (squareNum == 6) {
+    t2.center = CGPointMake(255.5, 529.5);
+  } else if (squareNum == 7) {
+    t2.center = CGPointMake(352.5, 529.5);
+  } else if (squareNum == 8) {
+    t2.center = CGPointMake(61.5, 620.5);
+  } else if (squareNum == 9) {
+    t2.center = CGPointMake(158.5, 620.5);
+  } else if (squareNum == 10) {
+    t2.center = CGPointMake(255.5, 620.5);
+  } else if (squareNum == 11) {
+    t2.center = CGPointMake(352.5, 620.5);
+  } else if (squareNum == 12) {
+    t2.center = CGPointMake(61.5, 711.5);
+  } else if (squareNum == 13) {
+    t2.center = CGPointMake(158.5, 711.5);
+  } else if (squareNum == 14) {
+    t2.center = CGPointMake(255.5, 711.5);
+  } else {
+    t2.center = CGPointMake(352.5, 711.5);
+  }
+}
+
+-(void)initSpawn {
+  int squareNum = arc4random_uniform(16);
+  
+  if(squareNum == 0) {
+    t1.center = CGPointMake(61.5, 438.5);
+  } else if (squareNum == 1) {
+    t1.center = CGPointMake(158.5, 438.5);
+  } else if (squareNum == 2) {
+    t1.center = CGPointMake(255.5, 438.5);
+  } else if (squareNum == 3) {
+    t1.center = CGPointMake(352.5, 438.5);
+  } else if (squareNum == 4) {
+    t1.center = CGPointMake(61.5, 529.5);
+  } else if (squareNum == 5) {
+    t1.center = CGPointMake(158.5, 529.5);
+  } else if (squareNum == 6) {
+    t1.center = CGPointMake(255.5, 529.5);
+  } else if (squareNum == 7) {
+    t1.center = CGPointMake(352.5, 529.5);
+  } else if (squareNum == 8) {
+    t1.center = CGPointMake(61.5, 620.5);
+  } else if (squareNum == 9) {
+    t1.center = CGPointMake(158.5, 620.5);
+  } else if (squareNum == 10) {
+    t1.center = CGPointMake(255.5, 620.5);
+  } else if (squareNum == 11) {
+    t1.center = CGPointMake(352.5, 620.5);
+  } else if (squareNum == 12) {
+    t1.center = CGPointMake(61.5, 711.5);
+  } else if (squareNum == 13) {
+    t1.center = CGPointMake(158.5, 711.5);
+  } else if (squareNum == 14) {
+    t1.center = CGPointMake(255.5, 711.5);
+  } else {
+    t1.center = CGPointMake(352.5, 711.5);
+  }
 }
 
 
